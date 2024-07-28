97bb6740f5959544ce12f66dc3d38c30bf7ae840
chandan kumar <ckumar2@deloitte.com>
2024-07-28 16:04:40 +0530

    initial commit of email service

---
+++ src/test/java/com/chandan/emailservice/EmailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chandan.emailservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
